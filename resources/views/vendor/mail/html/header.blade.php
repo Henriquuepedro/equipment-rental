1a5457d95e7101d7a4fdb1f4713b755f08362342
Pedro Henrique <pedrohenrique.sc.96@gmail.com>
2024-02-25 20:42:08 -0300

    Ajuste e-mail de boas vindas 2.

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ public_path(env('app.logotipo')) }}" class="logo" alt="{{ env('app.name') }}"> {{ $slot }}
+<img src="{{ url(config('app.logotipo')) }}" class="logo" alt="{{ config('app.name') }}"> {{ $slot }}
 </a>
 </td>
 </tr>
