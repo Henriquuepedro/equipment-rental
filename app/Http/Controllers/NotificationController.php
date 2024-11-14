50483cdfee9ec7c6588367a73870e5ebfe7742a0
Pedro Henrique <pedrohenrique.sc.96@gmail.com>
2024-11-13 21:59:39 -0300

    Notificação visão admin

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -11,6 +11,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class NotificationController extends Controller
 {
@@ -72,6 +73,16 @@ class NotificationController extends Controller
             $filter_default[]['where']['function'] = function($query) use ($company_id) {
                 $query->where('company_id', null)
                     ->orWhere('company_id', $company_id);
+            };
+
+            $filter_default[]['where']['function'] = function($query) use ($company_id) {
+                $query->where('expires_in', null)
+                    ->orWhere('expires_in', '>', dateNowInternational());
+            };
+
+            $filter_default[]['where']['function'] = function($query) use ($company_id) {
+                $query->where('only_permission', null)
+                    ->orWhereIn('only_permission', getAllPermissions());
             };
 
             if (!is_null($read) && $read !== 'all') {
